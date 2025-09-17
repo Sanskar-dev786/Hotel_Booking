db8eda6eb511a8976ed85827de32b1905e79c8e6
Nandani1806pandey <your_email@example.com>
2025-09-17 21:05:27 +0530

    Navbar was created successfully and the login functionaliey will we implemented with the help of clark

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import Navbar from "./compenents/Navbar";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 //import { useLocation } from "react-router-dom";
 
 const App = () => {
-  // const isOwnerPath = useLocation().pathname.includes("owner");
+  //const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
-      <Navbar />
+      {<Navbar />}
+      <div className="min-h-[70vh">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ import router
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-createRoot(document.getElementById('root')).render(
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
+);
