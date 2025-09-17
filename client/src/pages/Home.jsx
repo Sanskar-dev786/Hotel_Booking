fb41cad64b0078cc2e4cc7b7bc8db00d6cdd8bfb
Nandani1806pandey <your_email@example.com>
2025-09-18 00:05:19 +0530

    Destination section and exlusive offer section now created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
+import FeaturedDestination from "../components/FeaturedDestination";
+import ExcluseiveOffers from "../components/ExcluseiveOffers";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedDestination />
+      <ExcluseiveOffers />
     </>
   );
 };
